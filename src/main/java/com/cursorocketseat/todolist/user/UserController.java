78a69df3afb0b55e6b9e560055d801906bb95224
Lucas Danilo <lukastecno@gmail.com>
2023-10-13 22:48:10 -0300

    Response Entity

--- src/main/java/com/cursorocketseat/todolist/user/UserController.java
+++ src/main/java/com/cursorocketseat/todolist/user/UserController.java
@@ -2,6 +2,8 @@ package com.cursorocketseat.todolist.user;
 
 import com.cursorocketseat.todolist.repository.IUserRepositoryUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +18,15 @@ public class UserController {
 
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
         if (user != null) {
             System.out.println("Usuario Já Existe");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já Existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
 
     }
