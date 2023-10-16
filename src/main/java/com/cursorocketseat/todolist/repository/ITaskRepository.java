48104dc96083c896ae4fa1e1925eddb54eff5a41
Lucas Danilo <lukastecno@gmail.com>
2023-10-16 01:20:08 -0300

    Lista de Tarefas

--- src/main/java/com/cursorocketseat/todolist/repository/ITaskRepository.java
+++ src/main/java/com/cursorocketseat/todolist/repository/ITaskRepository.java
@@ -3,7 +3,10 @@ package com.cursorocketseat.todolist.repository;
 import com.cursorocketseat.todolist.taskmodel.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser (UUID idUser);
 }
--- src/main/java/com/cursorocketseat/todolist/taskmodel/tascontroller/TaskController.java
+++ src/main/java/com/cursorocketseat/todolist/taskmodel/tascontroller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -36,6 +37,14 @@ public class TaskController {
 
         var task = this.iTaskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.iTaskRepository.findByIdUser((UUID) idUser);
+        return tasks;
 
     }
 
